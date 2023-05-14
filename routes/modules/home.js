c52f778af1f9d102a300ef371cded72032f90705
rr37 <ay870527@gmail.com>
2023-05-14 19:29:16 +0800

    feat: add hpme page and detail page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const Todo = db.Todo
+const User = db.User
 
-router.use('/', (req, res) => {
-  res.send('Hello World')
+router.get('/', (req, res) => {
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => { return res.render('index', { todos: todos }) })
+    .catch((error) => { return res.status(422).json(error) })
 })
 
 module.exports = router
